c738c7a47f4bdd7b3364c60b001610ce5d985dce
whchen <whchen@qq.com>
2017-03-08 02:58:56 +0000

    more fiel

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+
+if __name__ == '__main__':
+    setup(name='DCGAN-tensorflow', 
+        packages=['DCGAN-tensorflow'], install_requires=["scikit-image", "matplotlib", "scipy"])
+
+
